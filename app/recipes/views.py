356600bf642d2a67bbb6189d95e2b816eaa42d8c
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-06-04 21:18:43 +0200

    update the home page

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.templatetags.static import static
 
 def home(response):
     return render(response, "home.html", {})
