9daa2764415ce7d78a66c93456bc4ed9183e16c6
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-06-05 23:05:12 +0200

    start creating model

--- app/recipes/models.py
+++ app/recipes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipes(models.Model):
+    title = models.CharField(max_length=150)
+    discription = models.TextField()
+    instructions = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "f{self.title} - {self.discription} - {self.created_by.username}"
+    
