bd17764d60eb88d0b03a7b9d2dcdb37d46ad831e
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-06-06 22:02:53 +0200

    registered models to admin

--- app/recipes/admin.py
+++ app/recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import * 
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Recipes)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
+admin.site.register(FavoriteRecipe)
+admin.site.register(Rating)
