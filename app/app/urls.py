c58c1aade485a5aefde19709e323c5f52f27f2f0
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-06-15 09:33:22 +0200

    start register page

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as v
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("recipes.urls")),
+    path("register/", v.registretion, name="register"),
+    path("", include("django.contrib.auth.urls")),
 ]
