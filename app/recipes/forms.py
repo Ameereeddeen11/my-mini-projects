0bf8b587b3383d7f3e1c9c2b34f3ad11943c8457
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-06-19 20:59:19 +0200

    create forms

--- app/recipes/forms.py
+++ app/recipes/forms.py
@@ -1,7 +1,7 @@
 from django import forms 
 from .models import *
 
-class Recipe(forms.ModelForm):
+class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipes
         fields = [
@@ -10,4 +10,26 @@ class Recipe(forms.ModelForm):
             "ingredient",
             "instructions",
             "category",
+        ]
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        field = "name"
+
+class FavoriteRecipeForm(forms.Model):
+    class Meta:
+        model = FavoriteRecipe
+        fields = [
+            "user_id",
+            "recipe_id",
+        ]
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = [
+            "user_id",
+            "recipe_id",
+            "score",
         ]
