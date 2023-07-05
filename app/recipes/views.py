d99ecb623c922b96d34be81648046394842ccbee
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-07-05 13:52:58 +0200

    uploading file is working

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -20,14 +20,14 @@ def create(request):
         if form_recipe.is_valid():
             fr = form_recipe.save()
             # URL adress to AWS S3 images
-            s3_client = boto3.client('s3')
-            bucket_name = 'myminiprojectrecipe'
-            file_key = str(fr.image)
-            url = s3_client.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': file_key}, ExpiresIn=3600)
-            fr.url = url
+            #s3_client = boto3.client('s3')
+            #bucket_name = 'myminiprojectrecipe'
+            #file_key = str(fr.image)
+            #url = s3_client.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': file_key}, ExpiresIn=3600)
+            #fr.url_image = url
             fr.save()
             #RecipeOwner.objects.create(recipe_id=fr, created_by=request.user)
-        return redirect("home.html")
+        return redirect("home")
     else:
         form_recipe = RecipeForm()
         #form_image = ImageForm()
