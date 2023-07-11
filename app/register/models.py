bc648271ce19b2ae3fe7dce7f77cc6604f248ef0
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-07-11 20:22:56 +0200

    create importing profile picture

--- app/register/models.py
+++ app/register/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    profile_pic = models.ImageField(upload_to="images/")
+    profile_pic = models.ImageField(upload_to="images/profile-pic/")
     #follow = models.ManyToManyField(User)
