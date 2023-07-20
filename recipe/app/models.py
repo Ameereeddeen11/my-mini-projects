7d4e2db3aa604a86636e3fcc7b80e797bc497aec
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-07-20 08:01:37 +0200

    created models

--- recipe/app/models.py
+++ recipe/app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipes(models.Model):
+    title = models.CharField(max_length=150)
+    discription = models.TextField()
+    ingredient = models.TextField(null=True)
+    instructions = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "f{self.title} - {self.discription}"
+
+
+class ImagesRecipesOwner(models.Model):
+    recipe_id = models.ForeignKey(Recipes, on_delete=models.CASCADE, null=True)
+    image = models.FileField(upload_to='images/recipe/', default='unkown-profile.jpg', null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.created_by
+
+
+class FavoriteRecipe(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe_id = models.ForeignKey(Recipes, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user_id.username} - {self.recipe.title}"
+
+
+class Rating(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe_id = models.ForeignKey(Recipes, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.user_id.username} - {self.recipe_id.title({self.score})}'
