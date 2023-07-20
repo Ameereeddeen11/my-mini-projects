33a4c21b0be36ce609456ac3cbf5973a049cf437
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-07-20 19:35:59 +0200

    add models to admin site

--- recipe/app/admin.py
+++ recipe/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+from register.models import Profile
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(ImagesRecipesOwner)
+admin.site.register(Category)
+admin.site.register(Rating)
+admin.site.register(Recipes)
+admin.site.register(FavoriteRecipe)
--- recipe/app/views.py
+++ recipe/app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from register.models import Profile
+from .models import *
 
 def home(response):
     profile = Profile.objects.all()
-    return render(response, "home.html", {"profile":profile})
+    recipe = ImagesRecipesOwner.objects.all()
+    return render(response, "home.html", {
+        "profile":profile,
+        "recipe": recipe
+    })
