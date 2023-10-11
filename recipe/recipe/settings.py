68aa8a28190b7e5700c51cc8afef5a7fd5b0b426
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-10-11 20:02:02 +0200

    setting up statics

--- recipe/recipe/settings.py
+++ recipe/recipe/settings.py
@@ -169,3 +169,5 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazon.com" % AWS_STORAGE_BUCKET_NAME
 AWS_S3_FILE_OVERWRITE = False
+# Toto vypne sběr statických souborů
+AWS_PRELOAD_METADATA = True
